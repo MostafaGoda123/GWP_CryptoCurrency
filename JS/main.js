a043cf34dfe1092fbfec67602d7bcb76e56b7e03
Eng\ Mostafa El-Sayed <104257870+MostafaGoda123@users.noreply.github.com>
2024-05-15 13:28:22 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+let btc = document.getElementById("btc") ;
+let eth = document.getElementById("eth") ;
+let doge = document.getElementById("doge") ;
+let settings = {
+   "async" : true , 
+   "scrossDomain" : true ,
+   "url" : "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+   "method" : "GET",
+   "headers" : {}
+}
+$.ajax(settings).done(function (res) {
+   btc.innerHTML = `$${res.bitcoin.usd}`
+   eth.innerHTML = `$${res.ethereum.usd}`
+   doge.innerHTML = `$${res.dogecoin.usd}`
+})
+
